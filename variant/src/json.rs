3162f541f2c8db06999037eed7ea8b80fd1f3ba9
Harsh Motwani <harsh.motwani@databricks.com>
2025-06-20 18:03:41 -0700

    removed Rc Refcell dependence

--- variant/src/json.rs
+++ variant/src/json.rs
@@ -1,22 +1,19 @@
 //! Tools for working with JSON strings and Variants
 
 use serde_json::Value;
-use std::cell::RefCell;
 use std::collections::HashMap;
 use std::error::Error;
-use std::rc::Rc;
 use std::usize;
 use crate::variant_utils;
-use crate::memory_allocator::{MemoryAllocator, SampleMemoryAllocator};
+use crate::memory_allocator::MemoryAllocator;
 
 const DEFAULT_SIZE_LIMIT: usize = 16 * 1024 * 1024;
 
-struct VariantBuilder<T: MemoryAllocator> {
-    value: Rc<RefCell<Box<[u8]>>>,
+struct VariantBuilder<'a, T: MemoryAllocator> {
     size: usize,
     size_limit: usize,
     dictionary: HashMap<String, usize>,
-    memory_allocator: T,
+    memory_allocator: &'a mut T,
 }
 
 struct FieldEntry<'a> {
@@ -25,7 +22,7 @@ struct FieldEntry<'a> {
     offset: usize
 }
 
-impl<T: MemoryAllocator> VariantBuilder<T> {
+impl<'a, T: MemoryAllocator> VariantBuilder<'a, T> {
     fn build(&mut self, json: &Value) -> Result<(), Box<dyn Error>> {
         match json {
             Value::Null => self.append_null(),
@@ -79,13 +76,11 @@ impl<T: MemoryAllocator> VariantBuilder<T> {
             // TODO: Formalize this error.
             return Err("Variant size limit exceeded.".into());
         }
-        let cur_len = self.value.borrow().len();
+        let cur_len = self.memory_allocator.borrow_value_buffer().len();
         if required > cur_len {
             // Need to get new buffer
             let new_size = required.next_power_of_two();
-            let old_value = self.value.clone();
-            self.value = self.memory_allocator.get_buffer(new_size)?;
-            self.value.borrow_mut()[0..self.size].copy_from_slice(&old_value.borrow()[0..self.size]);
+            self.memory_allocator.ensure_value_buffer_size(new_size)?;
         }
         Ok(())
     }
@@ -155,16 +150,16 @@ impl<T: MemoryAllocator> VariantBuilder<T> {
         self.check_capacity(header_size)?;
         self.shift_bytes(start + header_size, start, start + data_size)?;
         let offset_start = start + 1 + size_bytes;
-        let mut borrowed_value = self.value.borrow_mut();
-        borrowed_value[start] = self.array_header(large_size, offset_size as u8);
-        borrowed_value[start + 1..offset_start].copy_from_slice(&num_offsets.to_le_bytes()[..size_bytes]);
+        let value_buffer = self.memory_allocator.borrow_value_buffer();
+        value_buffer[start] = Self::array_header(large_size, offset_size as u8);
+        value_buffer[start + 1..offset_start].copy_from_slice(&num_offsets.to_le_bytes()[..size_bytes]);
         let mut offset_itr = offset_start;
         for offset in offsets {
-            borrowed_value[offset_itr..offset_itr+offset_size]
+            value_buffer[offset_itr..offset_itr+offset_size]
                 .copy_from_slice(&offset.to_le_bytes()[..offset_size]);
             offset_itr += offset_size;
         }
-        borrowed_value[offset_itr..offset_itr+offset_size]
+        value_buffer[offset_itr..offset_itr+offset_size]
             .copy_from_slice(&data_size.to_le_bytes()[..offset_size]);
         Ok(())
     }
@@ -181,7 +176,6 @@ impl<T: MemoryAllocator> VariantBuilder<T> {
     }
 
     fn array_header(
-        &self,
         large_size: bool,
         offset_size: u8,
     ) -> u8 {
@@ -214,25 +208,23 @@ impl<T: MemoryAllocator> VariantBuilder<T> {
         let header_size = 1 + size_bytes + num_fields * id_size + (num_fields + 1) * offset_size;
         self.check_capacity(header_size)?;
         self.shift_bytes(start + header_size, start, start + data_size)?;
-        let mut borrowed_value = self.value.borrow_mut();
-        borrowed_value[start] = self.object_header(large_size, id_size as u8, offset_size as u8);
+        let value_buffer = self.memory_allocator.borrow_value_buffer();
+        value_buffer[start] = Self::object_header(large_size, id_size as u8, offset_size as u8);
         let id_start = start + 1 + size_bytes;
         let offset_start = id_start + num_fields * id_size;
         if large_size {
-            borrowed_value[start + 1..id_start]
+            value_buffer[start + 1..id_start]
                 .copy_from_slice(&(num_fields as u32).to_le_bytes());
         } else {
-            borrowed_value[start + 1..id_start]
+            value_buffer[start + 1..id_start]
                 .copy_from_slice(&(num_fields as u8).to_le_bytes());
         }
-        std::mem::drop(borrowed_value);
         self.write_field_ids_and_offsets(id_start, id_size, offset_start, offset_size,
             data_size, fields.as_slice());
         Ok(())
     }
 
     fn object_header(
-        &self,
         large_size: bool,
         id_size: u8,
         offset_size: u8,
@@ -254,16 +246,16 @@ impl<T: MemoryAllocator> VariantBuilder<T> {
     ) {
         let mut id_itr = id_start;
         let mut offset_itr = offset_start;
-        let mut borrowed_value = self.value.borrow_mut();
+        let value_buffer = self.memory_allocator.borrow_value_buffer();
         for field in fields {
-            borrowed_value[id_itr..id_itr + id_size]
+            value_buffer[id_itr..id_itr + id_size]
                 .copy_from_slice(&(field.id).to_le_bytes()[..id_size]);
-            borrowed_value[offset_itr..offset_itr + offset_size]
+            value_buffer[offset_itr..offset_itr + offset_size]
                 .copy_from_slice(&(field.offset).to_le_bytes()[..offset_size]);
             id_itr += id_size;
             offset_itr += offset_size;
         }
-        borrowed_value[offset_itr..offset_itr + id_size]
+        value_buffer[offset_itr..offset_itr + id_size]
             .copy_from_slice(&(data_size).to_le_bytes()[..offset_size]);
     }
 
@@ -278,12 +270,12 @@ impl<T: MemoryAllocator> VariantBuilder<T> {
     }
 
     fn write_bytes(&mut self, bytes: &[u8]) -> Result<(), Box<dyn Error>> {
-        let mut borrowed_value = self.value.borrow_mut();
-        if self.size + bytes.len() > borrowed_value.len() {
+        let value_buffer = self.memory_allocator.borrow_value_buffer();
+        if self.size + bytes.len() > value_buffer.len() {
             // Formalize this error
             return Err("Buffer size insufficient. There might be a bug in the memory allocator.".into());
         }
-        borrowed_value[self.size..self.size + bytes.len()].copy_from_slice(bytes);
+        value_buffer[self.size..self.size + bytes.len()].copy_from_slice(bytes);
         self.size += bytes.len();
         Ok(())
     }
@@ -295,7 +287,7 @@ impl<T: MemoryAllocator> VariantBuilder<T> {
         end: usize,
     ) -> Result<(), Box<dyn Error>> {
         let additional = new_start - start;
-        let mut borrowed_value = self.value.borrow_mut();
+        let borrowed_value = self.memory_allocator.borrow_value_buffer();
         if self.size + additional > borrowed_value.len() {
             return Err("Buffer size limit exceeded".into());
         }
@@ -333,19 +325,14 @@ impl<T: MemoryAllocator> VariantBuilder<T> {
 /// Constructs a variant representation from a json string `json` (assumed to be valid utf-8) and
 /// writes the "value" and "metadata" fields of the variant into `value` and `metadata` buffers
 /// respectively.
-pub fn json_to_variant(
-    value: &mut Rc<RefCell<Box<[u8]>>>,
-    metadata: &mut [u8],
-    value_size: &mut usize,
+pub fn json_to_variant<T: MemoryAllocator>(
     json: &str,
+    memory_allocator: &mut T,
+    value_size: &mut usize,
 ) -> Result<(), Box<dyn Error>> {
     let json: Value = serde_json::from_str(json)?;
-    let memory_allocator = SampleMemoryAllocator {
-        buffer: value.clone()
-    };
 
     let mut vb = VariantBuilder {
-        value: value.clone(),
         size: 0,
         dictionary: HashMap::new(),
         size_limit: DEFAULT_SIZE_LIMIT,
@@ -353,25 +340,26 @@ pub fn json_to_variant(
     };
     vb.build(&json)?;
     *value_size = vb.size;
-    *value = vb.memory_allocator.buffer;
     Ok(())
 }
 
 #[cfg(test)]
 mod tests {
     use std::error::Error;
-    use std::cell::RefCell;
-    use std::rc::Rc;
     use crate::json::json_to_variant;
+    use crate::memory_allocator::SampleMemoryAllocator;
 
     #[test]
     fn test_json_to_variant() -> Result<(), Box<dyn Error>> {
         fn compare_results(json: &str, expected_value: &[u8]) -> Result<(), Box<dyn Error>> {
             let json = json;
-            let mut value_buffer: Rc<RefCell<Box<[u8]>>> = Rc::new(RefCell::new(Box::new([0u8; 1])));
             let mut value_size: usize = 0;
-            json_to_variant(&mut value_buffer, &mut [1, 2, 3, 4, 5], &mut value_size, json)?;
-            let computed_slize: &[u8] = &*value_buffer.borrow();
+
+            let mut memory_allocator = SampleMemoryAllocator {
+                value_buffer: vec![0u8; 1].into_boxed_slice()
+            };
+            json_to_variant(json, &mut memory_allocator, &mut value_size)?;
+            let computed_slize: &[u8] = &*memory_allocator.value_buffer;
             assert_eq!(&computed_slize[..value_size], expected_value);
             Ok(())
         }
--- variant/src/memory_allocator.rs
+++ variant/src/memory_allocator.rs
@@ -3,21 +3,46 @@ use std::error::Error;
 use std::rc::Rc;
 
 pub trait MemoryAllocator {
-    fn get_buffer(&mut self, size: usize) -> Result<Rc<RefCell<Box<[u8]>>>, Box<dyn Error>>;
+    /// Returns the slice where value needs to be written to. This method may be called several
+    /// times during the construction of a new `value` field in a variant. The implementation must
+    /// make sure that on every call, all the data written to the value buffer written so far are
+    /// preserved.
+    fn borrow_value_buffer(&mut self) -> &mut [u8];
+
+    /// Ensures that the next call to `borrow_value_buffer` returns a slice having at least `size`
+    /// bytes. Also ensures that the value bytes written so far are persisted - this means that
+    /// if `borrow_value_buffer` is to written a new buffer from the next call onwards, the new
+    /// buffer must have the contents of the old value buffer.
+    fn ensure_value_buffer_size(&mut self, size: usize) -> Result<(), Box<dyn Error>>;
 }
 
 pub struct SampleMemoryAllocator {
-    pub buffer: Rc<RefCell<Box<[u8]>>>,
+    pub value_buffer: Box<[u8]>,
 }
 
 impl MemoryAllocator for SampleMemoryAllocator {
-    fn get_buffer(&mut self, size: usize) -> Result<Rc<RefCell<Box<[u8]>>>, Box<dyn Error>> {
-        let cur_len = self.buffer.borrow().len();
-        if size > cur_len {
-            // reallocate buffer
-            let new_buffer = Rc::new(RefCell::new(vec![0u8; size].into_boxed_slice()));
-            self.buffer = new_buffer;
-        }
-        Ok(self.buffer.clone())
+    fn borrow_value_buffer(&mut self) -> &mut [u8] {
+        return &mut *self.value_buffer;
     }
+
+    fn ensure_value_buffer_size(&mut self, size: usize) -> Result<(), Box<dyn Error>> {
+        let cur_len = self.value_buffer.len();
+        if size > cur_len {
+            // Reallocate larger buffer
+            let mut new_buffer = vec![0u8; size].into_boxed_slice();
+            new_buffer[..cur_len].copy_from_slice(&self.value_buffer);
+            self.value_buffer = new_buffer;
+        }
+        Ok(())
+    }
+
+    // fn get_buffer(&mut self, size: usize) -> Result<&mut [u8], Box<dyn Error>> {
+    //     let cur_len = self.buffer.len();
+    //     if size > cur_len {
+    //         // reallocate buffer
+    //         let new_buffer = vec![0u8; size].into_boxed_slice();
+    //         self.buffer = new_buffer;
+    //     }
+    //     Ok(&mut *self.buffer)
+    // }
 }
